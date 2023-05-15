1e1f9a7cd759b7ce456f8eaa6486ac06ff8b6635
Skrivni <133706494+Skrivni@users.noreply.github.com>
2023-05-15 23:29:10 +0200

    Update build.py
    optimized functions import, +documentation&optimization for PY3.5+ in process()

--- build.py
+++ build.py
@@ -2,18 +2,24 @@
 # -*- coding: utf-8  -*-
 
 from __future__ import print_function
-import os
-import subprocess
+from os import path, walk, version_info #Per PEP20
+from subprocess import run, check_output #Per PEP20, +faster pre-processing
 
 def process(*args):
+    #Handler function for check_output run command
+    #Input: string arguments
+    #Output: encoded byte flow
     print(*args)
-    content = subprocess.check_output(args)
+    if version_info.major >= 3 and version_info.minor >=5:
+        content = run(args) #since PY3.5 subprocess.run is preferred
+    else:
+        content = check_outputs(args) #back-compatibility with PY<3.5
 
 def main():
-    root = os.path.join(os.path.dirname(__file__), "static")
-    for dirpath, dirnames, filenames in os.walk(root):
+    root = path.join(path.dirname(__file__), "static")
+    for dirpath, dirnames, filenames in walk(root):
         for filename in filenames:
-            name = os.path.relpath(os.path.join(dirpath, filename))
+            name = path.relpath(path.join(dirpath, filename))
             if filename.endswith(".js") and ".min." not in filename:
                 process("uglifyjs", "--compress", "-o", name.replace(".js", ".min.js"), "--", name)
             if filename.endswith(".css") and ".min." not in filename:
